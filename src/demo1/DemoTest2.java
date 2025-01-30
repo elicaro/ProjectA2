418cea119c977381417ac638bf10293d1a5706ee
carol <carol@10.0.0.241>
2025-01-29 21:09:42 -0500

    add int

--- src/demo1/DemoTest2.java
+++ src/demo1/DemoTest2.java
@@ -4,6 +4,19 @@ public class DemoTest2 {
 
 	public static void main (String args[]) {
 		System.out.println("Java");
+		
+		int a = 100;
+		int b = 76;
+		
+		int result = a + b;
+		
+		System.out.println(result);
+		
 	}
 	
-}
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
+	}
+	
+
